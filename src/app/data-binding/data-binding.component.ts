31426962dd2786b9a4619bace4cbc94b937b31e3
vroxo <vitor.icnv@gmail.com>
2017-03-28 14:41:33 -0300

    Testes com data-binding e import do bootstrap

---
+++ src/app/data-binding/data-binding.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-data-binding',
+  templateUrl: './data-binding.component.html',
+  styleUrls: ['./data-binding.component.css']
+})
+export class DataBindingComponent implements OnInit {
+  url: string = 'http://loiane.com';
+  cursoAngular: boolean = true;
+  urlImagens = 'http://lorempixel.com/400/200/sports/';
+ 
+  getValor(){
+    return 1;
+  }
+
+  getCurtirCurso(){
+    return true;
+  }
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
